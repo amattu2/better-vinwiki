6c3a3e37065f5d5feaec2e53d86aa996b6645df7
Alec M <alecmattu1234@gmail.com>
2023-09-04 10:30:34 -0400

    feat: Add post image expanding
    Signed-off-by: Alec M <alecmattu1234@gmail.com>

--- src/components/FeedPost/Image.tsx
+++ src/components/FeedPost/Image.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
-import { OpenInNew } from "@mui/icons-material";
-import { Box, Card, CardContent, Grid, Stack, Typography, styled } from "@mui/material";
+import React, { FC, useState } from "react";
+import { AspectRatio, OpenInNew } from "@mui/icons-material";
+import { Box, Card, CardContent, Grid, Modal, Stack, Typography, styled } from "@mui/material";
 import { Link } from "react-router-dom";
 import useProgressiveQuality from "../../hooks/useProgressiveQuality";
 import PostComments from "./Components/PostComments";
@@ -25,6 +25,9 @@ const StyledImageBox = styled(Box)({
   borderRadius: "8px",
   position: "relative",
   background: "#ddd",
+  "&:hover .expand-button": {
+    opacity: 1,
+  },
 });
 
 const StyledBackground = styled("div", {
@@ -41,9 +44,9 @@ const StyledBackground = styled("div", {
   right: 0,
   bottom: 0,
   zIndex: 1,
-  transition: "filter 0.3s ease-out, transform 0.3s ease-out",
+  transition: "filter 0.3s ease-out",
   "&:hover": {
-    transform: "scale(1.1)",
+    filter: "brightness(0.8)"
   },
 }));
 
@@ -55,38 +58,61 @@ const StyledLink = styled(Link)({
   color: "#838383",
   transition: "opacity 0.3s ease-out",
   opacity: 0,
+  zIndex: 2,
+});
+
+const StyledButton = styled("div")({
+  position: "absolute",
+  right: "8px",
+  top: "8px",
+  cursor: "pointer",
+  color: "#fff",
+  opacity: 0,
+  transition: "opacity 0.3s ease-out",
+  zIndex: 2,
 });
 
 const ImagePost: FC<FeedPost> = (post: FeedPost) => {
   const { uuid, image, comment_count, post_text } = post;
   const [src, { blur }] = useProgressiveQuality(image?.thumb, image?.large);
 
+  const [open, setOpen] = useState(false);
+
   return (
-    <StyledCard elevation={0}>
-      <CardContent>
-        <Grid container>
-          <Grid item xs={8}>
-            {/* TODO: Add expand icon on hover and click to expand */}
-            <StyledImageBox>
-              <StyledBackground bg={src} blur={blur} />
-            </StyledImageBox>
+    <>
+      <StyledCard elevation={0}>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={8}>
+              <StyledImageBox className="image-box">
+                <StyledBackground bg={src} blur={blur} />
+                <StyledButton className="expand-button" onClick={() => setOpen(true)}>
+                  <AspectRatio />
+                </StyledButton>
+              </StyledImageBox>
+            </Grid>
+            <Grid item xs={4}>
+              <Stack gap={1.5}>
+                <ProfileBit post={post} />
+                <GenericText content={post_text} />
+                <Typography variant="body2" color="textSecondary" fontSize={12} fontWeight={600}>
+                  {formatDateTime(new Date(post.post_date))}
+                </Typography>
+              </Stack>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Stack gap={1.5}>
-              <ProfileBit post={post} />
-              <GenericText content={post_text} />
-              <Typography variant="body2" color="textSecondary" fontSize={12} fontWeight={600}>
-                {formatDateTime(new Date(post.post_date))}
-              </Typography>
-            </Stack>
-          </Grid>
-        </Grid>
-        <PostComments key={uuid} uuid={uuid} count={comment_count} />
-      </CardContent>
-      <StyledLink to={`/post/${uuid}`} target="_blank" className="external-button">
-        <OpenInNew />
-      </StyledLink>
-    </StyledCard>
+          <PostComments key={uuid} uuid={uuid} count={comment_count} />
+        </CardContent>
+        <StyledLink to={`/post/${uuid}`} target="_blank" className="external-button">
+          <OpenInNew />
+        </StyledLink>
+      </StyledCard>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+          <img src={src} alt={post_text} style={{ maxHeight: "80vh", maxWidth: "80vw" }} />
+        </Box>
+      </Modal>
+    </>
   );
 };
 
