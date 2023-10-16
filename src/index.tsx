d239551156c10042c12f9697615c7104ff31761b
Alec M <alecmattu1234@gmail.com>
2023-10-16 19:27:19 -0400

    cs: Refactor routing architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
-import React, { FC, Suspense, lazy } from 'react';
+import React, { FC } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Box, CssBaseline, Stack, ThemeProvider } from '@mui/material';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { useReadLocalStorage, useDarkMode } from 'usehooks-ts';
-import { AuthProvider } from './Providers/AuthProvider';
-import { NotificationCountProvider } from './Providers/NotificationCountProvider';
-import AutoScroll from './components/ScrollToTop/AutoScroll';
-import Sidebar from './components/Sidebar';
-import { CacheKeys } from './config/Cache';
+import { useDarkMode } from 'usehooks-ts';
 import reportWebVitals from './reportWebVitals';
-import { CONFIG } from './config/AppConfig';
-import Loader from './components/Loader';
 import { DarkTheme } from './themes/dark';
 import { LightTheme } from './themes/light';
+import { routes } from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -35,47 +29,7 @@ declare module '@mui/material/styles' {
   }
 }
 
-const Login = lazy(() => import('./pages/login/Controller'));
-const Register = lazy(() => import('./pages/register/Controller'));
-const ForgotPassword = lazy(() => import('./pages/forgotPassword/Controller'));
-const Logout = lazy(() => import('./pages/logout/Controller'));
-const Home = lazy(() => import('./pages/index'));
-const Documentation = lazy(() => import('./pages/documentation'));
-const Lists = lazy(() => import('./pages/lists/Controller'));
-const Post = lazy(() => import('./pages/post/Controller'));
-const Profile = lazy(() => import('./pages/profile/Controller'));
-const Search = lazy(() => import('./pages/search/Controller'));
-const Vehicle = lazy(() => import('./pages/vehicle/Controller'));
-
-const ProtectedRoutes = () => {
-  const token = useReadLocalStorage<string>("token");
-  const profile = useReadLocalStorage<AuthProfile | null>(CacheKeys.AUTH_PROFILE);
-  if (!token || !profile?.uuid) {
-    return <Navigate to="/login" />;
-  }
-
-  return (
-    <AuthProvider>
-      <NotificationCountProvider>
-        <AutoScroll />
-        <Stack direction="row">
-          <Sidebar />
-          <Box sx={{ flexGrow: 1, ml: "72px" }}>
-            <Routes>
-              <Route path="/profile/:uuid?" element={<Profile />} />
-              <Route path="/vehicle/:vin" element={<Vehicle />} />
-              <Route path="/list/:uuid?" element={<Lists />} />
-              <Route path="/lists" element={<Lists />} />
-              <Route path="/post/:uuid" element={<Post />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </Box>
-        </Stack>
-      </NotificationCountProvider>
-    </AuthProvider>
-  );
-};
+const router = createBrowserRouter(routes);
 
 const App: FC = () => {
   const { isDarkMode } = useDarkMode();
@@ -83,18 +37,7 @@ const App: FC = () => {
   return (
     <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
       <CssBaseline />
-      <Router basename={CONFIG.PUBLIC_URL}>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/documentation" element={<Documentation />} />
-            <Route path="*" element={<ProtectedRoutes />} />
-          </Routes>
-        </Suspense>
-      </Router>
+      <RouterProvider router={router} fallbackElement={<p>Error!</p>} />
     </ThemeProvider>
   );
 };
---
+++ src/layouts/AuthenticatedLayout.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { Box, Stack } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import AutoScroll from '../components/ScrollToTop/AutoScroll';
+import Sidebar from '../components/Sidebar';
+
+/**
+ * The layout for a Authenticated user
+ *
+ * @returns {JSX.Element}
+ */
+export const AuthenticatedLayout: FC = () => (
+  <>
+    <AutoScroll />
+    <Stack direction="row">
+      <Sidebar />
+      <Box sx={{ flexGrow: 1, ml: "72px" }}>
+        <Outlet />
+      </Box>
+    </Stack>
+  </>
+);
