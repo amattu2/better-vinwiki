b979da1bc0213bee6584fbcaefbf7a03af92321f
Alec M. <alecmattu1234@gmail.com>
2024-10-13 10:16:28 -0400

    fix: CI tests failing

--- packages/backend/src/index.test.ts
+++ packages/backend/src/index.test.ts
@@ -1,19 +1,78 @@
-import { describe, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import router from "./index";
 
-describe("Routing", () => {
-	it.todo("should return a 404 error if the route is not found");
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-	it.todo(
-		"should return a 405 error if the method is not allowed for the route"
-	);
+const env: Env = {};
 
-	it.todo(
-		"should return a 500 error if an internal server error occurs"
-	);
+const ctx: ExecutionContext = {
+	waitUntil: (promise: Promise<any>): void => {
+		throw new Error("Function not implemented.");
+	},
+	passThroughOnException: (): void => {
+		throw new Error("Function not implemented.");
+	}
+};
 
-	it.todo("should return a 200 response if the route is found");
+describe("Basic Functionality", () => {
+	it("should return a 404 error if the route is not found", async () => {
+		const request = new IncomingRequest("https://mock-test-url/this-route-does-not-exist", {
+			method: "GET",
+		});
+
+		const response = await router.fetch(request, env, ctx);
+
+		expect(response).toMatchObject({ status: 404 });
+	});
+
+	it("should return a 405 error if the method is not allowed for the route", async () => {
+		const request = new IncomingRequest("https://mock-test-url/this-route-does-not-exist", {
+			method: "DELETE",
+		});
+
+		const response = await router.fetch(request, env, ctx);
+
+		expect(response).toMatchObject({ status: 405 });
+	});
 });
 
 describe("Golo365 Route", () => {
-	it.todo("should handle a valid request to the route");
+	it("should handle a valid request to the route", async () => {
+		const request = new IncomingRequest("https://mock-test-url/api/v1/vin/ZARFANBN2R7681933/golo365", {
+			method: "GET",
+		});
+
+		const response = await router.fetch(request, env, ctx);
+
+		expect(response).toMatchObject({ status: 200 });
+
+		// TODO: Mock handler to avoid actually calling external service
+	});
+
+	it("should return a 400 error if the VIN is invalid", async () => {
+		const request = new IncomingRequest("https://mock-test-url/api/v1/vin/INVALIDVIN/golo365", {
+			method: "GET",
+		});
+
+		const response = await router.fetch(request, env, ctx);
+
+		expect(response).toMatchObject({ status: 400 });
+	});
+
+	it.each<string>([
+		"POST",
+		"DELETE",
+		"HEAD",
+		"PUT",
+		"PATCH",
+		"OPTIONS",
+	])("should return a 405 error if the method is not allowed for the route", async (method) => {
+		const request = new IncomingRequest("https://mock-test-url/api/v1/vin/ZARFANBN2R7681933/golo365", {
+			method,
+		});
+
+		const response = await router.fetch(request, env, ctx);
+
+		expect(response).toMatchObject({ status: 405 });
+	});
 });
--- packages/backend/src/middleware/withVinValidation.test.ts
+++ packages/backend/src/middleware/withVinValidation.test.ts
@@ -1,13 +1,44 @@
-import { describe, it } from 'vitest';
-
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+import { describe, expect, it, vi } from 'vitest';
+import withVinValidation from './withVinValidation';
+import { Router } from 'itty-router';
 
 describe('withVinValidation Middleware', () => {
-	it.todo("should forward the request if the VIN is valid");
+	it.each<string>([
+		"ZHWGC7AJ3ELA13895",
+		"2T2HA31U74C006100",
+		"5YJSA1E5XNF479116",
+	])("should forward the request if the VIN is valid", async (vin) => {
+		const router = Router();
+		const request = { method: 'GET', url: `https://mock-url-test.com/${vin}` };
+		const handler = vi.fn(({ vin }) => ({ vin }));
 
-	it.todo("should transform the VIN to uppercase if it's valid");
+		await router.get('/:vin', withVinValidation, handler).fetch(request);
 
-	it.todo("should return a 400 error if the VIN is not provided");
+		expect(handler).toHaveBeenCalledWith(expect.objectContaining({ params: { vin } }));
+	});
 
-	it.todo("should return a 400 error if the VIN is not 17 characters long");
+	it("should transform the VIN to uppercase if it's valid", async () => {
+		const router = Router();
+		const request = { method: 'GET', url: 'https://mock-url-test.com/5yjsa1e5xnf479116' };
+		const handler = vi.fn(({ vin }) => ({ vin }));
+
+		await router.get('/:vin', withVinValidation, handler).fetch(request);
+
+		expect(handler).toHaveBeenCalledWith(expect.objectContaining({ params: { vin: '5YJSA1E5XNF479116' } }));
+	});
+
+	it.each<string>([
+		"not a vin",
+		"ZHWGC7AJ3ELA1389",
+		"-",
+	])("should return a 400 error if the VIN is not 17 characters long", async (vin) => {
+		const router = Router();
+		const request = { method: 'GET', url: `https://mock-url-test.com/${vin}` };
+		const handler = vi.fn(({ vin }) => ({ vin }));
+
+		const response = await router.get('/:vin', withVinValidation, handler).fetch(request);
+
+		expect(response).toMatchObject({ status: 400 });
+		expect(handler).not.toHaveBeenCalled();
+	});
 });
